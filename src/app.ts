5b0aa40f83753c9acf54cdaa69fbdab50f9fca63
alberto.niironen <alberto.niironen@gmail.com>
2023-10-02 18:33:51 +0100

    [ADD] Basic express setup

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+const spain = require("../GeoJSON/countries/spain.json");
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.disable("x-powered-by");
+
+// Middleware
+app.use((req: Request, res: Response, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  next();
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Welcome to Express & TypeScript Server");
+});
+
+app.get("/spain", (req: Request, res: Response) => {
+  res.status(200).json(spain);
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).send("404 Not Found");
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
