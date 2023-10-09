5593aa4bec5ede83bc2b7b16441000b85518828b
alberto.niironen <alberto.niironen@gmail.com>
2023-10-09 13:05:27 +0100

    [ADD] Tests are working

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,27 +1,37 @@
 import type { Config } from "jest";
+import type { JestConfigWithTsJest } from "ts-jest";
 
-const config: Config = {
+const config: JestConfigWithTsJest = {
   verbose: true,
   clearMocks: true, // Automatically clear mock calls and instances between every test
   collectCoverage: true, // Indicates which folders/files to cover
   coverageDirectory: "coverage", // The directory where Jest should output its coverage files
   coverageProvider: "v8", // Indicates which provider should be used to instrument code for coverage
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"], // An array of file extensions your modules use
+  preset: "ts-jest",
+  testEnvironment: "node", // The test environment that will be used for testing
 
-  roots: ["<rootDir>/src"], // The root directory that Jest should scan for tests and modules within
+  roots: ["<rootDir>"], // The root directory that Jest should scan for tests and modules within
 
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ], // The glob patterns Jest uses to detect test files
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest", // Use ts-jest for ts/tsx files
-  },
 
   extensionsToTreatAsEsm: [".ts"], // Important to active module as ECMAScript Modules -> https://jestjs.io/es-ES/docs/configuration#extensionstotreatasesm-arraystring
-
-  // Import statements in ESM are live bindings, so we need to tell Jest to hoist the exports
-  // https://jestjs.io/docs/ecmascript-modules#hoisting
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
 
 export default config;
--- src/routes/__tests__/countries.test.ts
+++ src/routes/__tests__/countries.test.ts
@@ -2,12 +2,45 @@ import app from "../../app";
 import request from "supertest";
 
 // The endpoints return geojson data
-describe("Test the root path", () => {
-  it("It should response the GET method", async () => {
-    const res = await request(app).get("/countries").expect(200);
-    expect(res.body.type).toEqual("FeatureCollection");
-    // res.body.features.forEach((feature: any) => {
-    //   expect(feature.type).toEqual("Feature");
-    // });
+describe("Countries", () => {
+  describe("Get all countries", () => {
+    it("It should response the GET method", async () => {
+      const res = await request(app).get("/countries");
+      const country_one = res.body[0];
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+      expect(country_one.type).toEqual("FeatureCollection");
+    });
+  });
+
+  describe("Countries is an array of Features", () => {
+    it("It should response the GET method", async () => {
+      const res = await request(app).get("/countries");
+      const country_one = res.body[0];
+
+      expect(res.statusCode).toBe(200);
+      expect(country_one.type).toEqual("FeatureCollection");
+    });
+  });
+
+  describe("Spain is a Country", () => {
+    it("It should response the GET method", async () => {
+      const res = await request(app).get("/countries/spain");
+      const spain = res.body;
+
+      expect(res.statusCode).toBe(200);
+      expect(spain.features[0].type).toEqual("Feature");
+    });
+  });
+
+  describe("Spain have three coordinates - Mainland, Balearic Islands and Canary Islands", () => {
+    it("It should response the GET method", async () => {
+      const res = await request(app).get("/countries/spain");
+      const spainFeatures = res.body.features;
+
+      expect(res.statusCode).toBe(200);
+      expect(spainFeatures.length).toEqual(3);
+    });
   });
 });
